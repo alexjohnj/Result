931389f99da74b0b2b4592c79f886559793f2088
Alex Jackson <alex@alexj.org>
2017-01-31 19:06:08 +0000

    Mega commit
    Whoops, forgot to commit as I went along.

--- Sources/Result.swift
+++ Sources/Result.swift
@@ -1,13 +1,75 @@
 public enum Result<T> {
     case ok(T)
     case fail(Error)
-}
 
-extension Result where T: Equatable {
-    static func ==(lhs: Result<T>, rhs: Result<T>) -> Bool {
-        switch (lhs, rhs) {
-        case (.ok(let left), .ok(let right)): return left == right
-        default: return false
+    // MARK: Initialization
+
+    public init(val: T) {
+        self = .ok(val)
+    }
+
+    public init(error: Error) {
+        self = .fail(error)
+    }
+
+    /// Initialise from an optional T wrapping the value if it is non-nil or `error` otherwise.
+    public init(_ val: T?, error: Error) {
+        switch val {
+        case .some(let wrapped): self = .ok(wrapped)
+        case .none: self = .fail(error)
+        }
+    }
+
+    /// Initialise from the result of a throwing function, wrapping the thrown error if the function throws.
+    public init(_ f: @autoclosure () throws -> T) {
+        self.init(try: f)
+    }
+
+    /// Initialise from the result of a throwing function, wrapping the thrown error if the function throws.
+    public init(try f: () throws -> T) {
+        do {
+            self = .ok(try f())
+        } catch let err {
+            self = .fail(err)
+        }
+    }
+
+    // MARK: Properties
+    /// Returns the wrapped value if it is wrapped by `.ok`. Otherwise returns `nil`.
+    public var value: T? {
+        get {
+            switch self {
+            case .ok(let val): return val
+            case .fail: return nil
+            }
+        }
+    }
+
+    // MARK: Devolution
+
+    /// Converts a `Result<T>` type to a `T` throwing if the value is `.fail`.
+    public func dematerialize() throws -> T {
+        switch self {
+        case .ok(let val): return val
+        case .fail(let error): throw(error)
+        }
+    }
+
+    // MARK: Monadic Functions
+
+    /// Applies the function `f` to a `Result<T>` if it is `.ok` and re-wraps the resulting value.
+    public func map<U>(_ f: ((T) -> U)) -> Result<U> {
+        switch self {
+        case .ok(let val): return .ok(f(val))
+        case .fail(let error): return .fail(error)
+        }
+    }
+
+    /// Applies the function `f` to a `Result<T>` if it is `.ok` to produce a new `Result<U>`.
+    public func flatMap<U>(_ f: ((T) -> Result<U>)) -> Result<U> {
+        switch self {
+        case .ok(let val): return f(val)
+        case .fail(let error): return .fail(error)
         }
     }
 }
--- Tests/ResultTests/ResultTests.swift
+++ Tests/ResultTests/ResultTests.swift
@@ -3,24 +3,105 @@ import XCTest
 
 enum TestError: Error {
     case GenericError
-    case AnotherGenericError
+}
+
+func helperThrowingFunction(shouldThrow: Bool) throws -> Int {
+    if shouldThrow {
+        throw(TestError.GenericError)
+    } else {
+        return 23
+    }
 }
 
 class ResultTests: XCTestCase {
-    func testResultOKEquality() {
-        let resultOne = Result<Int>.ok(23)
-        let resultTwo = Result<Int>.ok(23)
-        let resultThree = Result<Int>.ok(12)
-        XCTAssertTrue(resultOne == resultTwo)
-        XCTAssertFalse(resultOne == resultThree)
+    func testGetResultOptionalValue() {
+        let testResultNotNil = Result(val: 23)
+        XCTAssertNotNil(testResultNotNil.value)
+        XCTAssertEqual(testResultNotNil.value, 23)
+
+        let testResultNil: Result<Int> = Result(error: TestError.GenericError)
+        XCTAssertNil(testResultNil.value)
     }
 
-    func testResultFailEquality() {
-        let resultOne = Result<Int>.fail(TestError.GenericError)
-        let resultTwo = Result<Int>.ok(12)
-        let resultThree = Result<Int>.fail(TestError.AnotherGenericError)
+    func testInitResultFromNilOptional() {
+        let testValue: Int? = nil
+        let testResult = Result(testValue, error: TestError.GenericError)
 
-        XCTAssertFalse(resultOne == resultTwo)
-        XCTAssertFalse(resultOne == resultThree)
+        XCTAssertNil(testResult.value)
+    }
+
+    func testInitResultFromNonNilOptional() {
+        let testValue: Int? = 12
+        let testResult = Result(testValue, error: TestError.GenericError)
+
+        XCTAssertNotNil(testResult.value)
+    }
+
+    func testInitFromThrowingFunction() {
+        let testResult = Result(try: { try helperThrowingFunction(shouldThrow: true) })
+        XCTAssertNil(testResult.value)
+    }
+
+    func testInitFromNonThrowingFunction() {
+        let testResult = Result(try: { try helperThrowingFunction(shouldThrow: false) })
+        XCTAssertNotNil(testResult.value)
+    }
+
+    func testInitFromThrowingAutoclosure() {
+        let testResult = Result(try helperThrowingFunction(shouldThrow: true))
+        XCTAssertNil(testResult.value)
+    }
+
+    func testInitFromNonThrowingAutoclosure() {
+        let testResult = Result(try helperThrowingFunction(shouldThrow: false))
+        XCTAssertNotNil(testResult.value)
+    }
+
+    func testDematerializeThrows() {
+        let testResult: Result<Int> = Result(nil, error: TestError.GenericError)
+        XCTAssertThrowsError(try testResult.dematerialize()) { error in
+            guard case TestError.GenericError = error else {
+                return XCTFail()
+            }
+        }
+    }
+
+    func testDematerializeNonNilDoesntThrow() {
+        let testResult = Result(32, error: TestError.GenericError)
+        let unwrappedValue = try? testResult.dematerialize()
+        XCTAssertNotNil(unwrappedValue)
+        XCTAssertEqual(unwrappedValue, 32)
+    }
+
+    func testMapOKResult() {
+        let testResult = Result(val: 32)
+        let id = { (d: Int) -> Int in d }
+
+        let mappedResult = testResult.map(id)
+        XCTAssertEqual(mappedResult.value, testResult.value)
+    }
+
+    func testMapFailResult() {
+        let testResult: Result<Int> = Result(error: TestError.GenericError)
+        let id = { (d: Int) -> Int in d }
+
+        let mappedResult = testResult.map(id)
+        XCTAssertNil(mappedResult.value)
+    }
+
+    func testFlatMapOKResult() {
+        let testResult = Result(val: 23)
+        let id = { (d: Int) -> Result<Int> in .ok(d) }
+
+        let mappedResult = testResult.flatMap(id)
+        XCTAssertEqual(mappedResult.value, testResult.value)
+    }
+
+    func testFlatMapFailResult() {
+        let testResult: Result<Int> = Result(error: TestError.GenericError)
+        let id = { (d: Int) -> Result<Int> in .ok(d) }
+
+        let mappedValue = testResult.flatMap(id)
+        XCTAssertNil(mappedValue.value)
     }
 }
