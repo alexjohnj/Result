f3026d5386f37503eb6a50ddc5a388501b47c5f5
Alex Jackson <alex@alexj.org>
2017-01-31 16:17:08 +0000

    Add Result type with equality definition

--- Sources/Result.swift
+++ Sources/Result.swift
@@ -1,4 +1,13 @@
-struct Result {
+public enum Result<T> {
+    case ok(T)
+    case fail(Error)
+}
 
-    var text = "Hello, World!"
+extension Result where T: Equatable {
+    static func ==(lhs: Result<T>, rhs: Result<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.ok(let left), .ok(let right)): return left == right
+        default: return false
+        }
+    }
 }
--- Tests/ResultTests/ResultTests.swift
+++ Tests/ResultTests/ResultTests.swift
@@ -1,17 +1,26 @@
 import XCTest
 @testable import Result
 
+enum TestError: Error {
+    case GenericError
+    case AnotherGenericError
+}
+
 class ResultTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Result().text, "Hello, World!")
+    func testResultOKEquality() {
+        let resultOne = Result<Int>.ok(23)
+        let resultTwo = Result<Int>.ok(23)
+        let resultThree = Result<Int>.ok(12)
+        XCTAssertTrue(resultOne == resultTwo)
+        XCTAssertFalse(resultOne == resultThree)
     }
 
+    func testResultFailEquality() {
+        let resultOne = Result<Int>.fail(TestError.GenericError)
+        let resultTwo = Result<Int>.ok(12)
+        let resultThree = Result<Int>.fail(TestError.AnotherGenericError)
 
-    static var allTests : [(String, (ResultTests) -> () throws -> Void)] {
-        return [
-            ("testExample", testExample),
-        ]
+        XCTAssertFalse(resultOne == resultTwo)
+        XCTAssertFalse(resultOne == resultThree)
     }
 }
